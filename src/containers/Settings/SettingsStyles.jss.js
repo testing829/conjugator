9c9a5df57c12c23b437e04515d5f354680667916
nickoferrall <nickoferrall@gmail.com>
2019-10-25 21:20:53 +0100

    apparently update to settings styles

---
+++ src/containers/Settings/SettingsStyles.jss.js
@@ -0,0 +1,35 @@
+const styles = theme => ({
+  button: {
+    marginRight: 100,
+    marginTop: 20,
+    width: 120,
+    height: 40
+  },
+  buttonUpdated: {
+    marginRight: 100,
+    marginTop: 20,
+    width: 120,
+    height: 40,
+    backgroundColor: 'green',
+    '&:hover': {
+      backgroundColor: '#1B5E20'
+    }
+  },
+  paper: {
+    backgroundColor: '#fafafa',
+    margin: '60px auto',
+    padding: theme.spacing(2),
+    width: theme.spacing(140),
+    [theme.breakpoints.down('sm')]: {
+      width: theme.spacing(75)
+    }
+  },
+  tensesItem: {
+    width: '25%'
+  },
+  updateButton: {
+    margin: theme.spacing(1)
+  }
+});
+
+export default styles;
